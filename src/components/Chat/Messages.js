eddb3bfc0995645a53e75e76bf2f91a39479366e
afatihyavasi <afatihyavasi@gmail.com>
2021-04-25 00:45:19 +0300

    Fix open image new tab

--- src/components/Chat/Messages.js
+++ src/components/Chat/Messages.js
@@ -3,8 +3,7 @@ import {useFirebaseConnect} from "react-redux-firebase";
 import {useSelector} from "react-redux";
 import {
     Text, Flex, Avatar, Box, Divider,
-    useColorModeValue, Tag, TagLabel, Image, useDisclosure,
-    Modal, ModalOverlay, ModalCloseButton, ModalBody, ModalContent, ModalHeader
+    useColorModeValue, Tag, TagLabel, Image, Link
 } from '@chakra-ui/react';
 import moment from "moment";
 
@@ -21,7 +20,6 @@ const Messages = ({currentChannel}) => {
         storeAs: 'users',
     }])
 
-    const {isOpen, onOpen, onClose} = useDisclosure()
     const channelMessages = useSelector(state => state.firebase.ordered.channelMessages);
     const timeFromNow = timeStamp => moment(timeStamp).fromNow();
     const timeColor = useColorModeValue('gray.400', 'gray.500');
@@ -34,7 +32,7 @@ const Messages = ({currentChannel}) => {
             behavior: 'smooth',
             block: 'end'
         })
-    })
+    },[])
 
 
     return (
@@ -55,48 +53,23 @@ const Messages = ({currentChannel}) => {
                             </Flex>
 
                             <Flex>
-                                {isMedia(value) ? <Image htmlWidth={'400px'}
-                                                         htmlHeight={'300px'}
-                                                         rounded={'sm'}
-                                                         m={'5px'}
-                                                         src={value.image}
-                                                         alt={`${value.user.name} send it`}
-                                                         cursor={'pointer'}
-                                                         onClick={onOpen}
-                                    />
+                                {isMedia(value) ? <Link href={value.image} isExternal><Image htmlWidth={'400px'}
+                                                               htmlHeight={'300px'}
+                                                               rounded={'sm'}
+                                                               m={'5px'}
+                                                               src={value.image}
+                                                               alt={`${value.user.name} send it`}
+                                                               cursor={'pointer'}
+                                    /></Link>
                                     : <Text m={'5px'}>{value.content}</Text>
                                 }
                             </Flex>
                             <Divider/>
                         </Box>
-
-                        {/*Image full screen modal*/}
-                        <Modal onClose={onClose} size={'full'} isOpen={isOpen}>
-                            <ModalOverlay/>
-                            <ModalContent>
-                                <ModalHeader>
-                                    <Flex align={'center'}>
-                                        <Tag colorScheme={value.user.color} mx={'5px'}>
-                                            <Avatar bg={'gray.600'} size={'2xs'} ml={'2px'}/>
-                                            <TagLabel ml={'5px'}>{value.user.name}</TagLabel>
-                                        </Tag>
-
-                                        <Text fontSize={"xs"}
-                                              color={timeColor}>
-                                            {timeFromNow(value.timestamp)}</Text>
-                                    </Flex>
-                                </ModalHeader>
-                                <ModalCloseButton/>
-                                <ModalBody>
-                                    <Image src={value.image} boxSize={'full'}
-                                           alt={`${value.user.name} send it`}/>
-                                </ModalBody>
-                            </ModalContent>
-                        </Modal>
                     </>
                 )
             })}
-            <div ref={lastMessageRef}></div>
+            <div ref={lastMessageRef}></div>{/**/}
         </div>
 
     );

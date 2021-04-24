05f8b3adab836191b3b697355e00843fa622baa4
afatihyavasi <afatihyavasi@gmail.com>
2021-04-24 19:26:11 +0300

    Add image on chat , zoom image

--- src/components/Chat/Messages.js
+++ src/components/Chat/Messages.js
@@ -1,6 +1,10 @@
 import {useFirebaseConnect} from "react-redux-firebase";
 import {useSelector} from "react-redux";
-import {Text, Flex, Avatar, Box, Divider, useColorModeValue, Tag,TagLabel,Image} from '@chakra-ui/react';
+import {
+    Text, Flex, Avatar, Box, Divider,
+    useColorModeValue, Tag, TagLabel, Image, useDisclosure,
+    Modal, ModalOverlay, ModalCloseButton, ModalBody, ModalContent,ModalHeader
+} from '@chakra-ui/react';
 import moment from "moment";
 
 
@@ -12,46 +16,81 @@ const Messages = ({currentChannel}) => {
     }]);
 
     useFirebaseConnect([{
-        path:'users',
-        storeAs:'users',
+        path: 'users',
+        storeAs: 'users',
     }])
+    const {isOpen, onOpen, onClose} = useDisclosure()
 
     const channelMessages = useSelector(state => state.firebase.ordered.channelMessages);
-
     const timeFromNow = timeStamp => moment(timeStamp).fromNow();
-
-    const timeColor = useColorModeValue('gray.400','gray.500');
-
+    const timeColor = useColorModeValue('gray.400', 'gray.500');
     const isMedia = value => value.hasOwnProperty("image");
 
     return (
-        <div style={{overflow:'auto',width:'100%'}}>
+
+
+        <div style={{overflow: 'auto', width: '100%'}}>
             {channelMessages && channelMessages.map(({key, value}) => {
                 return (
-                    // message Author and timestamp
-                    <Box my={'5px'} key={key}>
-                        <Flex align={'center'}>
-                            <Tag colorScheme={value.user.color} mx={'5px'}>
-                                <Avatar bg={'gray.600'} size={'2xs'} ml={'2px'}/>
-                                <TagLabel ml={'5px'}>{value.user.name}</TagLabel>
-                            </Tag>
+                    <>
+                        {/*// message Author and timestamp*/}
+                        <Box my={'5px'} key={key}>
+                            <Flex align={'center'}>
+                                <Tag colorScheme={value.user.color} mx={'5px'}>
+                                    <Avatar bg={'gray.600'} size={'2xs'} ml={'2px'}/>
+                                    <TagLabel ml={'5px'}>{value.user.name}</TagLabel>
+                                </Tag>
 
-                            <Text  fontSize={"xs"}
-                                  color={timeColor}>
-                                {timeFromNow(value.timestamp)}</Text>
-                        </Flex>
+                                <Text fontSize={"xs"}
+                                      color={timeColor}>
+                                    {timeFromNow(value.timestamp)}</Text>
+                            </Flex>
 
-                        <Flex>
-                            {isMedia(value) ? <Image htmlWidth={'400px'} htmlHeight={'300px'} rounded={'sm'} src={value.image} alt={`${value.user.name} send it`} m={'5px'}/>
-                                : <Text m={'5px'}>{value.content}</Text>
-                            }
+                            <Flex>
+                                {isMedia(value) ? <Image htmlWidth={'400px'}
+                                                         htmlHeight={'300px'}
+                                                         rounded={'sm'}
+                                                         m={'5px'}
+                                                         src={value.image}
+                                                         alt={`${value.user.name} send it`}
+                                                         cursor={'pointer'}
+                                                         onClick={onOpen}
+                                    />
+                                    : <Text m={'5px'}>{value.content}</Text>
+                                }
 
-                        </Flex>
-                        <Divider/>
-                    </Box>
+                            </Flex>
+                            <Divider/>
+                        </Box>
+
+                        {/*Image full screen modal*/}
+                        <Modal onClose={onClose} size={'full'} isOpen={isOpen}>
+                            <ModalOverlay/>
+                            <ModalContent>
+                                <ModalHeader>
+                                    <Flex align={'center'}>
+                                        <Tag colorScheme={value.user.color} mx={'5px'}>
+                                            <Avatar bg={'gray.600'} size={'2xs'} ml={'2px'}/>
+                                            <TagLabel ml={'5px'}>{value.user.name}</TagLabel>
+                                        </Tag>
+
+                                        <Text fontSize={"xs"}
+                                              color={timeColor}>
+                                            {timeFromNow(value.timestamp)}</Text>
+                                    </Flex>
+                                </ModalHeader>
+                                <ModalCloseButton/>
+                                <ModalBody>
+                                    <Image src={value.image} boxSize={'full'}
+                                           alt={`${value.user.name} send it`}/>
+                                </ModalBody>
+                            </ModalContent>
+                        </Modal>
+                    </>
                 )
             })}
         </div>
+
     );
 };
 
